bcb9f1463f8c4dcd010d12980d87e64815c7d5af
qba667 <3341647+qba667@users.noreply.github.com>
2023-02-19 16:59:43 +0000

    building on rp4

--- src/Channel/ServiceChannel.cpp
+++ src/Channel/ServiceChannel.cpp
@@ -18,6 +18,7 @@
 
 #include <f1x/aasdk/IO/PromiseLink.hpp>
 #include <f1x/aasdk/Channel/ServiceChannel.hpp>
+#include <boost/asio/ts/io_context.hpp>
 
 namespace f1x
 {
@@ -38,7 +39,7 @@ ServiceChannel::ServiceChannel(boost::asio::io_service::strand& strand,
 
 void ServiceChannel::send(messenger::Message::Pointer message, SendPromise::Pointer promise)
 {
-    auto sendPromise = messenger::SendPromise::defer(strand_.get_io_service());
+    auto sendPromise = messenger::SendPromise::defer(strand_.context());
     io::PromiseLink<>::forward(*sendPromise, std::move(promise));
     messenger_->enqueueSend(std::move(message), std::move(sendPromise));
 }
